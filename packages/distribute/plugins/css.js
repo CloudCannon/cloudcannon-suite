8789bf7cc20e97198550698d0c50fbf49c8e0a87
George Phillips <georgepaulphillips@gmail.com>
2018-06-15 14:48:20 +1200

    new css parser lib

--- packages/distribute/plugins/css.js
+++ packages/distribute/plugins/css.js
@@ -1,6 +1,6 @@
 var gutil = require("gulp-util"),
 	through = require("through2").obj,
-	rewriteCssUrls = require("css-url-rewrite"),
+	rewriteCssUrls = require("cssurl").URLRewriter,
 	path = require("path");
 
 var IGNORE_URL_REGEX = /^([a-z]+\:|\/\/|\#)/;
@@ -18,17 +18,19 @@ module.exports = function (options) {
 		file.sitePath = "/" + file.path.substring(file.base.length);
 		file.sitePath = file.sitePath.replace(/\/index.html?/i, "/");
 
-
 		var css = file.contents.toString(encoding);
 
-		css = rewriteCssUrls(css, function rewrite(href) {
+		var rewriter = new URLRewriter(function(href) {
 			if (IGNORE_URL_REGEX.test(href)) {
 				return href;
 			}
+
 			return "/" + path.join(options.baseurl, href);
 		});
 
-		file.contents = new Buffer(css);
+		var rewritten = rewriter.rewrite(css);
+
+		file.contents = new Buffer(rewritten);
 		this.push(file);
 		callback();
 	});
