214d9ec90bd0853c690ea856970233e522afc8bb
Liam Bigelow <liambigelow@cloudcannon.com>
2019-01-30 13:14:48 +1300

    Adding version logging

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const c = require('ansi-colors');
+var pjson = require('./package.json');
+console.log(c.yellow(`Running ${pjson.name} ${pjson.version}`));
+
 module.exports = {
 	jekyllDev: require("./packages/jekyll-dev"),
 	jekyllDocs: require("./packages/jekyll-docs"),
